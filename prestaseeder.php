fbbbb0816f677e7db46c32b6c346a78fa75844c9
aironasstun <stunzenas.aironas@gmail.com>
2022-06-09 02:08:07 +0300

    Added "" around reference so SQL works correctly.

--- prestaseeder.php
+++ prestaseeder.php
@@ -241,9 +241,10 @@ class PrestaSeeder extends Module
         $reference = substr(($start_letter), 0, 1).substr(($number), 0, 8).
             '-'.substr(($letter), 0, 1);
 
+
         $response = Db::getInstance()->getValue('SELECT `id_product`
         FROM `'._DB_PREFIX_.'product`
-        WHERE `reference` = '.pSQL($reference));
+        WHERE `reference` = "'.pSQL($reference).'"');
 
         if ($response) {
             $this->getRandomReference();
